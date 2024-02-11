d8cfeeda3ce4aa9430fe01ebfc4eaec859362efa
Siddhi yadav <113456549+Siddhi1kcr@users.noreply.github.com>
2024-02-12 01:03:57 +0530

    Add user controller management endpoints

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -9,6 +9,9 @@ const subscriptionSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User"
     }
+},
+{
+    timestamps: true
 })
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
